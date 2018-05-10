fa3f64bcf2a2f37079aa2b63d2de5b5558d06447
zachsaegesser <zachsaegesser@gmail.com>
2018-05-10 13:39:38 -0400

    updated game_mechanics and added git ignore

---
+++ back_end/game_mechanics/main_run.js
@@ -0,0 +1,23 @@
+//require data folder
+  //user-pets-habitits functions
+//require cookie
+function main(){
+  //get userID from cookie
+
+  //get current time
+  //get old user time
+  //calc difference
+
+  //update values based on difference
+  //get pet_hunger
+  //calc new pet_hunger
+  //if < 0 kill pet => update alive to false
+  //update pet_hunger to new pet_hunger
+
+  //get thirst_hunger
+  //calc new thirst_hunger
+  //if < 0 kill pet => update alive to false
+  //update thirst_hunger to new thirst_hunger
+
+  //do all the stuff
+}
