f7a8cdf909017b51b3f1cff318872b7b12d2bc69
zachsaegesser <zachsaegesser@gmail.com>
2018-05-10 21:00:37 -0400

    first draft game_mechanics

--- back_end/game_mechanics/main_run.js
+++ back_end/game_mechanics/main_run.js
@@ -8,7 +8,7 @@ require("../database")
   //user-pets-habitits functions
 //require cookie
 async function main(){
-  console.log(database);
+  // console.log(database);
 
 
   // await database.user.addUser("ryan", "sucks");
@@ -16,9 +16,11 @@ async function main(){
   // console.log("Before Change: ");
   // console.log(userList);
 
-  let user = await database.user.adoptPet("7b7997a2-c0d2-4f8c-b27a-6a1d4b5b6310", "the_pet_id");
-  let userList = await database.user.getAllUsers();
-  console.log(userList);
+  // let user = await database.user.adoptPet("7b7997a2-c0d2-4f8c-b27a-6a1d4b5b6310", "the_pet_id");
+
+  // let user  = await database.user.notNewUser("f12e8284-83af-42c0-b2e1-88b9dbdfa859");
+  // let userList = await database.user.getAllUsers();
+  // console.log(userList);
   // console.log(user);
 
   // let user = await database.user.getUserById("7b7997a2-c0d2-4f8c-b27a-6a1d4b5b6310");
@@ -44,38 +46,59 @@ async function main(){
 
 
 // "7b7997a2-c0d2-4f8c-b27a-6a1d4b5b6310"    Test user ID
-  // let user = get userID from cookie
-  // let curr_time = Date.now();
-  // let last_time = user['lastlogin']
-  // let diff_hours = floor((curr_time-last_time)/3600000;
 
-  // let pet = database.pet.getPetById(user['petId'])
+  // database.pet.addPet("Spot", 3, "white");
+  // await database.user.adoptPet(user["_id"], "ff4a83aa-6df9-4692-86e4-2867445f8904");
 
-  // if(pet["sick"]){
-  //   diff_hours = diff_hours * 3
-  // }
 
-  // let hunger = pet['hunger']
-  // hunger -= (diff_hours*100/72)
-  // let thirst = pet['thirst']
-  // thirst -= (diff_hours*100/24)
-  // let mentalHealth =  pet['mentalHealth']
-  // mentalHealth = (diff_hours*100/8)
+  // let user = get userID from cookie //////////////TODO NEEDs to be from cookie
+  let user = await database.user.getUserById("7b7997a2-c0d2-4f8c-b27a-6a1d4b5b6310");
 
-  // if hunger <= 0 or thirst <=0 {
-  //   temp = database.pet.updateSpecPet(pet["_id"], {"alive": false})
-  // }
+  
+  let curr_time = Date.now();
+  let last_time = user['lastLogin'];
+  let diff_hours = Math.floor((curr_time-last_time)/3600000);
+  console.log(user);
+  console.log(curr_time);
+  console.log(last_time);
+  console.log(diff_hours);
 
-  // if(mentalHealth < 50){
-  //   if(getRandomInt(3) == 1){
-  //      temp = database.pet.updateSpecPet(pet["_id"], {"sick": true})
-  //   }
-  // }
-  // else {
-  //   if(getRandomInt(6) == 1){
-  //     temp = database.pet.updateSpecPet(pet["_id"], {"sick": true})
-  //   }
-  // }
+
+  let pet = await database.pet.getPetById(user['petId'])
+  console.log(pet);
+  if(pet["sick"]){
+    diff_hours = diff_hours * 3
+  }
+
+  let hunger = pet['hunger']
+  hunger -= (diff_hours*100/72)
+  let thirst = pet['thirst']
+  thirst -= (diff_hours*100/24)
+  let mentalHealth =  pet['mentalHealth']
+  mentalHealth -= (diff_hours*100/8)
+
+  console.log("Hunger: " + hunger);
+  console.log("Thirst: " + thirst);
+  console.log("mentalHealth: " + mentalHealth);
+
+
+
+  if(hunger <= 0 || thirst <=0){
+    temp = database.pet.updateSpecPet(pet["_id"], {"alive": false})
+  }
+
+  if(mentalHealth < 50){
+    if(getRandomInt(3) == 1){
+       temp = database.pet.updateSpecPet(pet["_id"], {"sick": true})
+    }
+  }
+  else {
+    if(getRandomInt(6) == 1){
+      temp = database.pet.updateSpecPet(pet["_id"], {"sick": true})
+    }
+  }
+
+  console.log(pet);
 
   //update values based on difference
   //get pet_hunger
