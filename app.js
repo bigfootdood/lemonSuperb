99af8a0f43611c3a1894e821c2ce3e255d599579
zachsaegesser <zachsaegesser@gmail.com>
2018-05-11 11:02:48 -0400

    added lots of stuff including starting passport

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 const static = express.static(__dirname + "/public");
+const passport = require("passport");
+const LocalStrategy = require('passport-local').Strategy;
+// const database = require("back_end/data");
+// const flash=require("connect-flash");
+
+
 
 const configRoutes = require("./routes");
 const exphbs = require("express-handlebars");
@@ -9,9 +15,31 @@ app.use(express.static('public'));
 app.use("/public", static);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use(passport.initialize());
+app.use(passport.session());
+// app.use(flash());
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
+
+passport.use(new LocalStrategy({
+  usernameField: 'loginEmail',
+  passwordField: 'password',
+  // passReqToCallback : true
+  },
+  function(username, password, done) {
+    User.findOne({ userName: username }, function (err, user) {
+      if (err) { return done(err); }
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username.' });
+      }
+      if (!user.validPassword(password)) {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+      return done(null, user);
+    });
+  }
+));
+
 
 configRoutes(app);
 
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const path = require("path");
 const back_end = require("../back_end/game_mechanics/main_run");
-
+const passport = require("passport");
 // var fs = require('fs');
 // var fileName = '../public/app.json';
 // var file = require(fileName);
@@ -20,9 +20,14 @@ const constructorMethod = app => {
   });
 
   //new user login routes
-  app.post("/new_user", function(req, res) {
-    res.redirect('/new_user');
-  });
+  // app.post("/new_user", function(req, res) {
+  //   res.redirect('/new_user');
+  // });
+  app.post("/new_user",
+    passport.authenticate('local', { successRedirect: '/new_user',
+                                     failureRedirect: '/new_user'
+                                     })
+);
   app.get("/new_user", (req, res) => {
     //res.sendFile(path.resolve("public/login2.html"));
     res.render('NewUserLogin');
