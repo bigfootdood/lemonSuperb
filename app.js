9fc25c62ed5e4e7f97b86c214ba375e043b24e71
zachsaegesser <zachsaegesser@gmail.com>
2018-05-11 22:59:43 -0400

    removed useless stuff from app.js

--- app.js
+++ app.js
@@ -5,11 +5,6 @@ const bcrypt = require("bcrypt");
 const path = require("path");
 const app = express();
 const static = express.static(__dirname + "/public");
-// const passport = require("passport");
-// const LocalStrategy = require('passport-local').Strategy;
-// const database = require("back_end/data");
-// const flash=require("connect-flash");
-
 
 
 const configRoutes = require("./routes");
@@ -19,31 +14,9 @@ app.use(cookieParse());
 app.use("/public", static);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(passport.initialize());
-// app.use(passport.session());
-// app.use(flash());
+
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
-
-// passport.use(new LocalStrategy({
-//   usernameField: 'loginEmail',
-//   passwordField: 'password',
-//   // passReqToCallback : true
-//   },
-//   function(username, password, done) {
-//     User.findOne({ userName: username }, function (err, user) {
-//       if (err) { return done(err); }
-//       if (!user) {
-//         return done(null, false, { message: 'Incorrect username.' });
-//       }
-//       if (!user.validPassword(password)) {
-//         return done(null, false, { message: 'Incorrect password.' });
-//       }
-//       return done(null, user);
-//     });
-//   }
-// ));
-
 
 configRoutes(app);
 
